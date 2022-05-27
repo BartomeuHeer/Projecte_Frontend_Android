21e30687e4dfbfdf0b889973eda7a05186c84e4e
Bartomeu Heer Salva <bheersalva@gmail.com>
2022-05-27 18:22:12 +0200

    added splash activity

---
+++ app/src/main/java/edu/upc/eetac/dsa/SplashActivity.java
@@ -0,0 +1,18 @@
+package edu.upc.eetac.dsa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
+        finish();
+    }
+}
